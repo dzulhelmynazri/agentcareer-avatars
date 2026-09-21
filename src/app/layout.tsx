097363b188b46dae5a826d7985ebb719d13dac4d
Dzulhelmy Bin Nazri <trial.dzulhelmy@gmail.com>
2026-09-21 21:53:37 +0800

    Remove Footer component from layout
    Removed the Footer component from the layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
 
-import { Footer } from "@/components/footer";
 import { Header } from "@/components/header";
 
 import "../index.css";
@@ -47,7 +46,6 @@ const RootLayout = ({
         <div className="flex min-h-screen flex-col">
           <Header />
           {children}
-          <Footer />
         </div>
       </NuqsAdapter>
     </body>
